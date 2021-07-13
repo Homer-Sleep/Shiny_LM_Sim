38a384a4b46abffc13c387abe748236b4a95b069
Dave <ryand1@goldmail.etsu.edu>
2021-07-12 23:26:46 -0400

    working function and plot for 2 main and interaction

--- Learn_Simulate_LM.R
+++ Learn_Simulate_LM.R
@@ -34,10 +34,70 @@ results = c(coefs, ps, Rsq)
 names(results) = c('b0_coef', 'b1_coef', 'b2_coef', 'b3_coef',
                    'b0_p', 'b1_p', 'b2_p', 'b3_p', 'rsq')
 
+# Pack into a function: 
+regression_sim = function(simNum, n, b0, b1, b2, b3, x1mean=0, x1_sd=1, err_mean=0, err_sd=1){
+  x1 = rnorm(n, mean = x1_sd)
+  x2 = sample(0:1, n, replace = TRUE)
+  
+  y = b0 + (b1* x1) + (b2 * x2) + (b3 * x1 * x2) + rnorm(n, mean = err_mean, sd = err_sd)
+  
+  model = lm(y~ x1 * x2)
+  summary(model)
+  #Pull info
+  output = summary(model)$coefficients
+  coefs = output[,1]
+  ps = output[,4]
+  Rsq = summary(model)$r.squared
+  
+  results = c(coefs, ps, Rsq)
+  names(results) = c('b0_coef', 'b1_coef', 'b2_coef', 'b3_coef',
+                     'b0_p', 'b1_p', 'b2_p', 'b3_p', 'rsq')
+  return(results)
+}
+
+regression_sim(1, 300, 0, .3, .2, .3)
+
+# now try with repeatable simulations
+library(plyr)
+num_sims = 1000 # number of simulations
+sims = ldply(1:num_sims, regression_sim, n=100, b0=0, b1=.3, b2=.2, b3=.3)
+
+# ldply repeats the function for the number supplied (num_sims) using the parms supplied
+# calculate the proportion of pvalues that are significant for each main effect:
+
+powerx1 = sum(sims$b1_p < 0.05)/nrow(sims)
+powerx2 = sum(sims$b2_p < 0.05)/nrow(sims)
+powerx3 = sum(sims$b3_p < 0.05)/nrow(sims)
+
+## Varying Parameters: now use a range of parameters. Namely sample size and effect size
+sample_sizes = c(50, 100, 200, 300, 500)
+results = NULL
+num_sims = 1000
+for (val in sample_sizes) {
+  sims = ldply(1:num_sims, regression_sim, n=val, b0=0, b1=.3, b2=.2, b3=.3)
+  sims$n = val
+  results = rbind(results, sims)
+  
+}
+
+# plot results:
+library(ggplot2)
+library(tidyr)
+library(tidyverse)
+
+power_ests = results %>%
+  group_by(n)%>%
+  summarize(power = sum(b3_p < .05)/n())
+
+ggplot(power_ests, aes(x=n, y=power))+
+  geom_point()+
+  geom_line()+
+  ylim(c(0,1))+
+  geom_hline(yintercept =  .8)+
+  theme_minimal()
 
 
-
-
+#####################
 # Notes from  http://ms.mcmaster.ca/~bolker/emdbook/chap5A.pdf
 
 # Simple example of linear model with normal distribution of error
